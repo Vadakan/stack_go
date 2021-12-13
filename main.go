3b8290d916b131ad85129abac7072d6795a6628b
Sundar Munichamy <msundarmunichamy@gmail.com>
2021-12-13 12:26:27 +0530

    created empty stack

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var stack []int
+
+func crateemptystack() []int {
+	return stack
+}
+
+func main() {
+
+	empty_stack := crateemptystack()
+
+	fmt.Println("empty stack :", empty_stack)
+
+}
